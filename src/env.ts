a2beee06522f8a3d9498e0fc00afb1d7680683e1
paul-r <paulrousseaucs@gmail.com>
2022-12-30 00:41:18 +0100

    (env) update key values

--- src/env.ts
+++ src/env.ts
@@ -1,2 +1,2 @@
-export const API_KEY_HERE = 'M424dexlyo9bcdV0-AmDMeRneS4MTYs5vzOj1wpOqLE'
-export const API_KEY_GOOGLE = 'AIzaSyDp2DESkESDQ7NELCa9AcbyDLYLrBhLlaY'
+export const API_KEY_HERE = "your key"
+export const API_KEY_GOOGLE = "your key"
